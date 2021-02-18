0577ecb76d18f5ae7c3a6f1a17185c249174cd75
Chris Johns <chrisfotosmusic@gmail.com>
2021-02-18 13:09:05 -0500

    commit

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 const cors = require('cors')
 
+//git
 
 // the __dirname is the current directory from where the script is running
 app.use(cors())
