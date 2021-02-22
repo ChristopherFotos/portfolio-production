ae686b4548ad1c4a3c3c303e394870b921b8fe53
Chris Johns <chrisfotosmusic@gmail.com>
2021-02-22 18:07:21 -0500

    add props demo

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
-// const express = require('express');
-// const path = require('path');
-// const port = process.env.PORT || 3000;
-// const app = express();
-// const cors = require('cors')
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 3000;
+const app = express();
+const cors = require('cors')
 
-// //git
+//git
 
-// // the __dirname is the current directory from where the script is running
-// app.use(cors())
-// app.use(express.static(__dirname));
+// the __dirname is the current directory from where the script is running
+app.use(cors())
+app.use(express.static(__dirname));
 
-// app.use(express.static(path.join(__dirname, 'build')));
-// app.use(express.static(path.join(__dirname, 'demos')));
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'demos')));
 
-// app.get('/', function (req, res) {
-//   // res.sendFile(path.join(__dirname, 'build', 'index.html'));
-//   console.log('route hit')
-//   res.send('hello')
-// });
+app.get('/', function (req, res) {
+  // res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  console.log('route hit')
+  res.send('hello')
+});
 
-// app.get('/propsdemo', function (req, res) {
-//   console.log('get props demo')
-//   res.sendFile(path.join(__dirname, 'demos', 'props-demo', 'index.html'));
-//   res.status(200)
-// });
+app.get('/propsdemo', function (req, res) {
+  console.log('get props demo')
+  res.sendFile(path.join(__dirname, 'demos', 'props-demo', 'index.html'));
+  res.status(200)
+});
 
-// app.listen(port);
+app.listen(port);
